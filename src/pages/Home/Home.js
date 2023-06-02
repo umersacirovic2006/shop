e131dcab108445827812813190d473064262f836
umersacirovic2006 <asacirovic90@gmail.com>
2023-06-02 09:02:25 +0200

    Done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 function Home() {
   return (
     <div className='Home'>
-        <h1>Some nice home page</h1>
-        <Link to='all-products'>
-            <button>Just opens the page...</button>
-        </Link>
+      <h1>Some nice home page</h1>
+      <Link to='all-products'>
+        <button>Just opens the page...</button>
+      </Link>
     </div>
   )
 }
